db5bb9ef723b5adc92399fe025b01986a11b3db0
Muhammad Muzammal <muzamal503@gmail.com>
2024-05-30 14:55:48 +0500

    udpate

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,281 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./portfolio.scss";
 import { AiFillGithub } from "react-icons/ai";
 import { BsBoxArrowUpRight } from "react-icons/bs";
-import web1 from "../../assets/web1.jpg";
-import web2 from "../../assets/web2.jpg";
-import web3 from "../../assets/web3.jpeg";
-import web4 from "../../assets/web4.jpg";
-import web5 from "../../assets/web5.jpeg";
-import web6 from "../../assets/web6.jpeg";
-import web7 from "../../assets/web7.jpeg";
-import web8 from "../../assets/web8.jpeg";
-import web9 from "../../assets/web9.jpeg";
+import { projects } from "../../data";
 
 function Portfolio() {
+  useEffect(() => {
+    const items = document.querySelectorAll(".content");
+
+    items.forEach((item, index) => {
+      if (index % 2 !== 0) {
+        item.classList.add("content_reversed");
+      }
+    });
+  }, [projects]);
+
   return (
     <div id="portfolio" className="portfolio">
       <div className="container">
         <span>Portfolio</span>
         <h2>Each project is a unique piece of development 🧩</h2>
-        <div className="content content_reversed">
-          <div className="right">
-            <h3>CAR RENTAL (FEBRUARY 2023) 🚗</h3>
-            <p>
-              A car rental website is an online platform that allows users to
-              rent cars for personal or business use. The website provides an
-              interface for searching, comparing, and reserving cars.
-            </p>
-            <div className="btns">
-              <div className="btn">React</div>
-              <div className="btn">SCSS</div>
-            </div>
-            <div className="btns2">
-              <div className="btn1">
-                <a href="https://car-rantel.netlify.app/" target="_blank">
-                  Live_Demo
-                </a>
-                <div className="icon box">
-                  <BsBoxArrowUpRight />
+        {projects.map((item) => (
+          <div key={item.id} className="content">
+            <div className="right">
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+              <div className="btns">
+                {item.techStack.map((teck, index) => (
+                  <div key={index} className="btn">
+                    {teck}
+                  </div>
+                ))}
+              </div>
+              <div className="btns2">
+                <div className="btn1">
+                  <a href={item.url} target="_blank">
+                    Live_Demo
+                  </a>
+                  <div className="icon box">
+                    <BsBoxArrowUpRight />
+                  </div>
+                </div>
+                <div className="btn1">
+                  <a href={item.code} target="_blank">
+                    Code
+                  </a>
+                  <div className="icon box">
+                    <AiFillGithub />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="left">
-            <a href="https://car-rantel.netlify.app/" target="_blank">
-              <img src={web1} alt="" />
-            </a>
-          </div>
-        </div>
-        <div className="content">
-          <div className="left">
-            <a href="">
-              <img src={web2} alt="" />
-            </a>
-          </div>
-          <div className="right">
-            <h3>Bus Reservation System 🚌</h3>
-            <p>
-              A bus reservation system is an online platform for booking bus
-              tickets, making travel arrangements easier by allowing users to
-              select seats, dates, and routes conveniently.
-            </p>
-            <div className="btns">
-              <div className="btn">React</div>
-              <div className="btn">MySQL</div>
-            </div>
-            <div className="btns2">
-              <div className="btn1">
-                <a>Not deployed</a>
-                <div className="icon box">
-                  <BsBoxArrowUpRight />
-                </div>
-              </div>
+            <div className="left">
+              <a href={item.url} target="_blank">
+                <img src={item.image} alt="" />
+              </a>
             </div>
           </div>
-        </div>
-        <div className="content content_reversed">
-          <div className="right">
-            <h3>E-Store 📲</h3>
-            <p>
-              E-Stores make life easier by providing easy access to various
-              products. It allow simple comparisons, secure payments, and home
-              delivery, saving time and effort compared to traditional shopping.
-            </p>
-            <div className="btns">
-              <div className="btn">React</div>
-              <div className="btn">SCSS</div>
-              <div className="btn">API</div>
-            </div>
-            <div className="btns2">
-              <div className="btn1">
-                <a href="https://e-sale.netlify.app/" target="_blank">
-                  Live_Demo
-                </a>
-                <div className="icon box">
-                  <BsBoxArrowUpRight />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="left">
-            <a href="https://e-sale.netlify.app/" target="_blank">
-              <img src={web4} alt="" />
-            </a>
-          </div>
-        </div>
-        <div className="content">
-          <div className="left">
-            <a href="https://mzml.netlify.app/" target="_blank">
-              <img src={web3} alt="" />
-            </a>
-          </div>
-          <div className="right">
-            <h3>Portfolio Website 🚩</h3>
-            <p>
-              I am a student of BS Computer Science at University of agriculture
-              Faisalabad. I always strive to learn new skills and improve where
-              I am lacking. My goal is to work with an organization.
-            </p>
-            <div className="btns">
-              <div className="btn">React</div>
-            </div>
-            <div className="btns2">
-              <div className="btn1">
-                <a href="https://mzml.netlify.app/" target="_blank">
-                  Live_Demo
-                </a>
-                <div className="icon box">
-                  <BsBoxArrowUpRight />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="content content_reversed">
-          <div className="right">
-            <h3>Gymate 💪</h3>
-            <p>
-              Get fit and healthy at our gym by joining today. Your fitness
-              journey starts here with expert trainers. Make fitness a seamless
-              part of your lifestyle through affordable and flexible membership
-              options.
-            </p>
-            <div className="btns">
-              <div className="btn">React</div>
-              <div className="btn">SCSS</div>
-              <div className="btn">MongoDB</div>
-            </div>
-            <div className="btns2">
-              <div className="btn1">
-                <a href="https://gmate.netlify.app/" target="_blank">
-                  Live_Demo
-                </a>
-                <div className="icon box">
-                  <BsBoxArrowUpRight />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="left">
-            <a href="https://gmate.netlify.app/" target="_blank">
-              <img src={web5} alt="" />
-            </a>
-          </div>
-        </div>
-        <div className="content">
-          <div className="left">
-            <a href="https://e-comerc.netlify.app/" target="_blank">
-              <img src={web6} alt="" />
-            </a>
-          </div>
-          <div className="right">
-            <h3>ECOMMERCE 🛒</h3>
-            <p>
-              With a focus on simplicity and clean design, this store prioritize
-              user experience, making it easy for customers to find and purchase
-              the products they need.
-            </p>
-            <div className="btns">
-              <div className="btn">React</div>
-              <div className="btn">Vanila CSS</div>
-            </div>
-            <div className="btns2">
-              <div className="btn1">
-                <a href="https://e-comerc.netlify.app/" target="_blank">
-                  Live_Demo
-                </a>
-                <div className="icon box">
-                  <BsBoxArrowUpRight />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="content content_reversed">
-          <div className="right">
-            <h3>COINDOM 💰</h3>
-            <p>
-              Coindom is a crypto app that allows users to search for
-              information about various cryptocurrencies in real-time.
-            </p>
-            <div className="btns">
-              <div className="btn">React</div>
-              <div className="btn">SCSS</div>
-            </div>
-            <div className="btns2">
-              <div className="btn1">
-                <a href="https://crypto-coindom.netlify.app/" target="_blank">
-                  Live_Demo
-                </a>
-                <div className="icon box">
-                  <BsBoxArrowUpRight />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="left">
-            <a href="https://crypto-coindom.netlify.app/" target="_blank">
-              <img src={web7} alt="" />
-            </a>
-          </div>
-        </div>
-        <div className="content">
-          <div className="left">
-            <a href="">
-              <img src={web8} alt="" />
-            </a>
-          </div>
-          <div className="right">
-            <h3>Blog app 📖</h3>
-            <p>
-              A personal blog is an online platform for sharing thoughts, ideas,
-              and experiences, making it easier for readers to connect with the
-              author and engage in discussions by providing a space for regular
-              updates and reflections.
-            </p>
-            <div className="btns">
-              <div className="btn">React</div>
-              <div className="btn">MySQL</div>
-            </div>
-            <div className="btns2">
-              <div className="btn1">
-                <a>Not deployed</a>
-                <div className="icon box">
-                  <BsBoxArrowUpRight />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="content content_reversed">
-          <div className="right">
-            <h3>Resturant app 👨‍🍳</h3>
-            <p>
-              A restaurant app is an online platform for browsing menus, making
-              reservations, and ordering food, making it easier for users to
-              discover and enjoy new dining experiences by providing convenient
-              access to information and services.
-            </p>
-            <div className="btns">
-              <div className="btn">React</div>
-              <div className="btn">MongoDB</div>
-            </div>
-            <div className="btns2">
-              <div className="btn1">
-                <a>Not deployed</a>
-                <div className="icon box">
-                  <BsBoxArrowUpRight />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="left">
-            <a href="https://crypto-coindom.netlify.app/" target="_blank">
-              <img src={web9} alt="" />
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,93 @@
+import img1 from './assets/web1.jpg'
+import img2 from './assets/web2.jpg'
+import img3 from './assets/web4.jpg'
+import img4 from './assets/web3.jpeg'
+import img5 from './assets/web5.jpeg'
+import img6 from './assets/web6.jpeg'
+import img7 from './assets/web7.jpeg'
+import img8 from './assets/web8.jpeg'
+import img9 from './assets/web9.jpeg'
+
+export const projects = [
+    {
+        id: 1,
+        title: "CAR RENTAL SYSTEM 🚗",
+        description: "A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for searching, comparing, and reserving cars.",
+        techStack: ["React.js", "SCSS"],
+        url: "https://car-rantel.netlify.app/",
+        image: img1,
+        code: "https://github.com/m-muzamal/car-rantel"
+    },
+    {
+        id: 2,
+        title: "BUS RESERVATION SYSTEM 🚌",
+        description: "A bus reservation system is an online platform for booking bus tickets, making travel arrangements easier by allowing users to select seats, dates, and routes conveniently.",
+        techStack: ["React.js", "SCSS", "MySQL"],
+        url: null,
+        image: img2,
+        code: "https://github.com/m-muzamal/fyp"
+    },
+    {
+        id: 3,
+        title: "E-STORE 📲",
+        description: "E-Stores make life easier by providing easy access to various products. It allow simple comparisons, secure payments, and home delivery, saving time and effort compared to traditional shopping.",
+        techStack: ["React.js", "SCSS", "API"],
+        url: "https://e-sale.netlify.app/",
+        image: img3,
+        code: "https://github.com/m-muzamal/e-store"
+    },
+    {
+        id: 4,
+        title: "PORTFOLIO WEBSITE 🚩",
+        description: "Developed a personalized portfolio showcasing a collection of my work, skills, and professional journey. I always strive to learn new skills and improve where I am lacking. My goal is to work with an organization.",
+        techStack: ["React.js", "CSS"],
+        url: "https://mzml.netlify.app/",
+        image: img4,
+        code: "https://github.com/m-muzamal/m-muzamal.github.io"
+    },
+    {
+        id: 5,
+        title: "GYMATE 💪",
+        description: "Get fit and healthy at our gym by joining today. Your fitness journey starts here with expert trainers. Make fitness a seamless part of your lifestyle through affordable and flexible membership options.",
+        techStack: ["React.js", "sCSS", "MySQL"],
+        url: "https://gmate.netlify.app/",
+        image: img5,
+        code: "https://github.com/m-muzamal/gymate"
+    },
+    {
+        id: 6,
+        title: "ECOMMERCE 🛒",
+        description: "With a focus on simplicity and clean design, this store prioritize user experience, making it easy for customers to find and purchase the products they need.",
+        techStack: ["React.js", "Vanila CSS"],
+        url: "https://e-comerc.netlify.app/",
+        image: img6,
+        code: "https://github.com/m-muzamal/eCommerce"
+    },
+    {
+        id: 7,
+        title: "COINDOM 💰",
+        description: "Coindom is a crypto app that allows users to search for information about various cryptocurrencies in real-time.",
+        techStack: ["React.js", "SCSS"],
+        url: "https://crypto-coindom.netlify.app/",
+        image: img7,
+        code: "https://github.com/m-muzamal/coindom"
+    },
+    {
+        id: 8,
+        title: "BLOG APP 📖",
+        description: "A personal blog is an online platform for sharing thoughts, ideas, and experiences, making it easier for readers to connect with the author and engage in discussions by providing a space for regular updates and reflections.",
+        techStack: ["React.js", "SCSS", "MySQL"],
+        url: "https://online-blog-app.netlify.app/",
+        image: img8,
+        code: "https://github.com/m-muzamal/bloge_app"
+    },
+    {
+        id: 9,
+        title: "RESTURANT APP 👨‍🍳",
+        description: "A restaurant app is an online platform for browsing menus, making reservations, and ordering food, making it easier for users to discover and enjoy new dining experiences by providing convenient access to information and services.",
+        techStack: ["React.js", "CSS", "MongoDB"],
+        url: "https://rasturant-app.netlify.app/",
+        image: img9,
+        code: "https://github.com/m-muzamal/rasturant"
+    },
+]
